71504d82f6f6aaa1ea8778f710c66c85442bc02d
antonio <antoniob6@outlook.com>
2018-11-14 17:07:10 +0200

    code upload
    moved the code to the repository

---
+++ code/Unity Project/Assets/cameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+	
+    private Rigidbody2D rb;
+	private static ILogger logger = Debug.unityLogger;
+
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void LateUpdate()
+    {
+        offset = transform.position - player.transform.position;
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
+        //rb.MovePosition(new Vector2(player.transform.position.x+offset.x, player.transform.position.y + offset.y));
+        //rb.AddRelativeForce(new Vector2(-1*offset.x, -1*offset.y));
+    }
+}
