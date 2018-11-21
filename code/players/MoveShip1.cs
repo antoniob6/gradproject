a3ef6c7bd7ace003ae68f1b18d276def6f8defe4
antoniob6 <37347940+antoniob6@users.noreply.github.com>
2018-11-21 20:19:03 +0200

    Add files via upload

---
+++ code/players/MoveShip1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MoveShip : NetworkBehaviour {
+
+	[SerializeField]
+	private float speed;
+
+	void FixedUpdate () {
+		if (this.isLocalPlayer) {
+			float movementx = Input.GetAxis ("Horizontal");
+            float movementy = Input.GetAxis("Vertical");
+            if(movementx!=0 || movementy!=0)
+              GetComponent<Rigidbody2D> ().velocity = new Vector2 (movementx * speed, movementy*speed);
+		}
+	}
+}
