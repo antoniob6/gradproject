931085209026d126c1b6367b316d2bb5d6d534dd
antoniob6 <37347940+antoniob6@users.noreply.github.com>
2018-12-25 13:25:38 +0200

    updated player scripts
    now these scripts can handle dynamic maps

---
+++ code/player2ndGen/planetaryGravity.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planetaryGravity : MonoBehaviour {
+    public GameObject center;
+    
+    public float gravity = 10f;
+
+
+    private Rigidbody2D m_Rigidbody2D;
+    // Use this for initialization
+    void Start () {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+        center=(GameObject.FindGameObjectsWithTag("GravityCenter"))[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (center == null)
+        {
+            center = (GameObject.FindGameObjectsWithTag("GravityCenter"))[0];
+        }
+		
+	}
+    void FixedUpdate()
+    {
+        addforce();
+    }
+
+    void addforce()
+    {
+        m_Rigidbody2D.AddForce((center.transform.position - transform.position).normalized * 30f);
+    }
+}
