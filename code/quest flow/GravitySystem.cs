b6ee8b82c391b2171df83cb7b7105a9b67a58cb5
antoniob6 <37347940+antoniob6@users.noreply.github.com>
2019-01-29 20:11:46 +0200

    Add files via upload

---
+++ code/quest flow/GravitySystem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySystem :MonoBehaviour {
+    public enum GravityType
+    {
+        Down,Up,ToCenter,ToOut
+    }
+    public GravityType gravity= GravityType.Down;
+
+}
